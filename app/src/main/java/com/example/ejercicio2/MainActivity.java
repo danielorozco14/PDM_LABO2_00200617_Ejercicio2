273268056ef7abf50e63987cf5a99a735268979b
danielorozco14 <00200617@uca.edu.sv>
2019-03-15 15:20:53 -0600

    Implementacion de interfaz y clickListener

--- app/src/main/java/com/example/ejercicio2/MainActivity.java
+++ app/src/main/java/com/example/ejercicio2/MainActivity.java
@@ -2,12 +2,55 @@ package com.example.ejercicio2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private GridLayout mGridLayout;
+    private ImageView img1,img2,img3,img4,img5,img6,img7,img8,img9;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mGridLayout=findViewById(R.id.gridLayout);
+
+        img1=findViewById(R.id.pic1);
+        img2=findViewById(R.id.pic2);
+        img3=findViewById(R.id.pic3);
+        img4=findViewById(R.id.pic4);
+        img5=findViewById(R.id.pic5);
+        img6=findViewById(R.id.pic6);
+        img7=findViewById(R.id.pic7);
+        img8=findViewById(R.id.pic8);
+        img9=findViewById(R.id.pic9);
+
+        img1.setOnClickListener(this);
+        img2.setOnClickListener(this);
+        img3.setOnClickListener(this);
+        img4.setOnClickListener(this);
+        img5.setOnClickListener(this);
+        img6.setOnClickListener(this);
+        img7.setOnClickListener(this);
+        img8.setOnClickListener(this);
+        img9.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int viewID=v.getId();
+        Log.i("id",viewID+"");
+
+        switch (viewID){
+            case R.id.pic1:
+                img1.setImageResource(R.drawable.paisaje1);
+                break;
+
+        }
     }
 }
